63d2f461b8c99a45b9177170c834a0482d34364f
Roxane <a12030167@unet.univie.ac.at>
2026-07-17 16:51:11 +0200

    removed sanization flag for setting aromaticity (-> created bugs later)

--- scripts/evaluation.py
+++ scripts/evaluation.py
@@ -31,7 +31,6 @@ from rdkit.Chem import (
     QED,
     rdDepictor,
     SanitizeMol,
-    SanitizeFlags,
     SDMolSupplier,
     SDWriter,
 )
@@ -664,7 +663,6 @@ def evaluate_subdirectory(
     if use_sdf:
         supplier = SDMolSupplier(str(subdir / "generated_mols.sdf"), removeHs=False, sanitize=False)
         mols = [mol for mol in supplier]
-        [SanitizeMol(mol, sanitizeOps=SanitizeFlags.SANITIZE_SETAROMATICITY) for mol in mols]
     else:
         builder = MoleculeBuilder(vocab=params["data_set"])
         x, pos, batch = builder.load_tensor_from_file(subdir)
@@ -682,8 +680,6 @@ def evaluate_subdirectory(
             mols = builder.generate_rdkit_molecules_via_xyz2mol(
                 x, pos, batch, progress_bar=True
             )
-
-        [SanitizeMol(mol, sanitizeOps=SanitizeFlags.SANITIZE_SETAROMATICITY) for mol in mols]
 
         save_molecules_to_sdf(mols, subdir / "generated_mols.sdf")
 
